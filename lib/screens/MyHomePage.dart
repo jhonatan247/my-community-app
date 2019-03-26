813e835236c5c00d7f363d8ec75d05174e7cef63
Santiago Velandia Casas <svelandiacasas@gmail.com>
2019-03-26 16:37:49 -0500

    navigationBar included

---
+++ lib/screens/MyHomePage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 1;
+  final _widgetOptions = [
+    Text('Index 0: Home'),
+    Text('Index 1: Business'),
+    Text('Index 2: School'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.question_answer, color: Colors.blue,), title: Text('Chat')),
+          BottomNavigationBarItem(icon: Icon(Icons.people, color: Colors.blue,), title: Text('Events')),
+          BottomNavigationBarItem(icon: Icon(Icons.settings, color: Colors.blue,), title: Text('Settings')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.indigo,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
