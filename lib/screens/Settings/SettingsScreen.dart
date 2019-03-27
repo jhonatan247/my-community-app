1d633ac6b4df59ed0d7ce3bddb1f2ac9a1e380b9
Santiago Velandia Casas <svelandiacasas@gmail.com>
2019-03-27 10:08:42 -0500

    Settings screen V1.0 created

--- lib/screens/Settings/SettingsScreen.dart
+++ lib/screens/Settings/SettingsScreen.dart
@@ -33,6 +33,58 @@ class SettingsScreen extends StatelessWidget{
                 child: editProfile,
               ),
             ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: SizedBox(
+                      height: 60,
+                      child: setStatus
+                    )
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 1, 0, 0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: SizedBox(
+                          height: 60,
+                          child: activity
+                      )
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 1, 0, 0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: SizedBox(
+                          height: 60,
+                          child: myFiles
+                      )
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 1, 0, 0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: SizedBox(
+                          height: 60,
+                          child: changeMood
+                      )
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
--- lib/screens/Settings/buttons.dart
+++ lib/screens/Settings/buttons.dart
@@ -5,3 +5,31 @@ var editProfile = FlatButton(
   child: Text('Edit Profile'),
   color: Color(0xffdedede),
 );
+var setStatus = RaisedButton.icon(
+    elevation: 0.3,
+    color: Colors.white,
+    onPressed: (){},
+    icon: Icon(Icons.party_mode),
+    label: Text('Set a status')
+);
+var activity = RaisedButton.icon(
+    elevation: 0.3,
+    color: Colors.white,
+    onPressed: (){},
+    icon: Icon(Icons.notifications),
+    label: Text('Activity')
+);
+var myFiles = RaisedButton.icon(
+    elevation: 0.3,
+    color: Colors.white,
+    onPressed: (){},
+    icon: Icon(Icons.attach_file),
+    label: Text('My files')
+);
+var changeMood = RaisedButton.icon(
+    elevation: 0.3,
+    color: Colors.white,
+    onPressed: (){},
+    icon: Icon(Icons.mood),
+    label: Text('Change Mood')
+);
