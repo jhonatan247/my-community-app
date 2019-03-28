8f4a01c43d4e05ad85b32e795fffbd7e9088a335
Andryut <aahuertasca@unal.edu.co>
2019-03-28 17:27:02 -0500

    the importa package name of the app changes from my_comminity to my_community.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:my_comminity/main.dart';
+import 'package:my_community/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
